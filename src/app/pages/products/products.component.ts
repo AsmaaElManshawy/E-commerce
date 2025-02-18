5b098c7b1c654275d27fd58f381463dae466125c
AhmedElmnshawy <ahmedfartaca4@gmail.com>
2025-02-19 01:17:59 +0200

    productepage

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { ProductsService } from '../../core/services/products/products.service';
+import { CardComponent } from '../../shared/components/ui/card/card.component';
+import { IProducts } from '../../shared/interfaces/IProducts/iproducts';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  private readonly productsService = inject(ProductsService);
+  products: WritableSignal<IProducts[]> = signal([]);
 
+  getProductsData(): void {
+    this.productsService.getAllProducts().subscribe({
+      next: (res) => {
+        console.log(res);
+        console.log(res.data);
+        this.products.set(res.data);
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.getProductsData();
+  }
 }
