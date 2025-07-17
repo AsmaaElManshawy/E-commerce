4e20bd2a46de180030729627dfbe65fab0801f1b
AsmaaElManshawy <sayedasmaa2001@gmail.com>
2025-07-17 20:26:41 +0300

    getPrerenderParams2

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,8 +1,10 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, inject } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { IProducts } from './shared/interfaces/IProducts/iproducts';
+import { ProductsService } from './core/services/products/products.service';
 
 const serverConfig: ApplicationConfig = {
   providers: [
@@ -12,3 +14,31 @@ const serverConfig: ApplicationConfig = {
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
+
+
+export async function getPrerenderParams() {
+  // Example: Fetch or define the list of valid IDs
+  const productsService = inject(ProductsService);
+  let products: IProducts[] = [];
+
+   function getProductsData(): void {
+    productsService.getAllProducts().subscribe({
+      next: (res) => {
+        console.log(res);
+        console.log(res.data);
+        products=res.data;
+      },
+    });
+  }
+  getProductsData();
+
+
+  // You can fetch these from an API or database
+
+  let ids = [];
+  for (let i = 0; i < products.length; i++) {
+    ids.push(products[i].id);
+  }
+
+  return ids.map(id => ({ id }));
+}
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,29 +10,4 @@ export const serverRoutes: ServerRoute[] = [
   }
 ];
 
-export async function getPrerenderParams() {
-  // Example: Fetch or define the list of valid IDs
-  const productsService = inject(ProductsService);
-  let products: IProducts[] = [];
 
-   function getProductsData(): void {
-    productsService.getAllProducts().subscribe({
-      next: (res) => {
-        console.log(res);
-        console.log(res.data);
-        products=res.data;
-      },
-    });
-  }
-  getProductsData();
-
-
-  // You can fetch these from an API or database
-
-  let ids = [];
-  for (let i = 0; i < products.length; i++) {
-    ids.push(products[i].id);
-  }
-
-  return ids.map(id => ({ id }));
-}
